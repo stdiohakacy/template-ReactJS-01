bb077b6e00f05954f8f9a27a00d1bdc9f534891b
Duy <huynhduytam2210@gmail.com>
2020-08-31 01:00:37 +0700

    withRouter()

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
-import React from 'react';
-import './App.css';
+import React from 'react'
+import './App.css'
+import { Route, Switch, Link } from 'react-router-dom'
+import HomePage from './pages/homepage/homepage.component'
 
-import HomePage from './pages/homepage/homepage.component';
+const HatsPage = () => (
+    <div>
+        Hats page
+    </div>
+)
 
 function App() {
     return (
         <div>
-            <HomePage />
+            <Switch>
+                <Route exact path='/' component={HomePage} />
+                <Route path='/hats' component={HatsPage} />
+            </Switch>
         </div>
-    );
+    )
 }
 
-export default App;
+export default App
