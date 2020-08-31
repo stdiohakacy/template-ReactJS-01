f7b0b0a7e73e35100d941f51f29bfccbb73f2e62
Duy <huynhduytam2210@gmail.com>
2020-08-31 11:19:24 +0700

    Collection Item

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,7 +1,6 @@
 import React, { Component } from 'react'
 import SHOP_DATA from './shop.data.js'
-import CollectionPreview from '../../components/preview-collection/preview-collection.component.jsx'
-
+import CollectionPreview from '../../components/collection-preview/collection-preview.component'
 class ShopPage extends Component {
     constructor(props) {
         super(props)
