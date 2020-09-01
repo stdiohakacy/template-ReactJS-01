30c633a4b1c3cf25fdbe99ff6ccf118fad5e6842
Duy <huynhduytam2210@gmail.com>
2020-09-01 21:24:33 +0700

    Cart Component

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -4,6 +4,7 @@ import { ReactComponent as Logo } from '../../assets/crown.svg'
 import { auth } from '../../firebase/firebase.utils'
 import './header.styles.scss'
 import { connect } from 'react-redux'
+import CartIcon from '../cart-icon/cart-icon.component'
 
 const Header = ({ currentUser }) => (
     <div className='header'>
@@ -22,6 +23,7 @@ const Header = ({ currentUser }) => (
                     ? (<div className='option' onClick={() => auth.signOut()}>Sign out</div>)
                     : (<Link className='option' to='/signin'>Sign in</Link>)
             }
+            <CartIcon />
         </div>
     </div>
 )
