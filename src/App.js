b08e80e6e76c3f789db06f2658326e18f8d7d073
Duy <huynhduytam2210@gmail.com>
2020-09-02 16:40:11 +0700

    Dispatch Action Shorthand

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import { connect } from 'react-redux'
 import { setCurrentUser } from './redux/user/user.actions'
 import { createStructuredSelector } from 'reselect'
 import { selectCurrentUser } from './redux/user/user.selectors'
+import CheckoutPage from './pages/checkout/checkout.component'
 
 class App extends Component {
     unsubscribeFromAuth = null
@@ -50,6 +51,7 @@ class App extends Component {
                 <Switch>
                     <Route exact path='/' component={HomePage} />
                     <Route path='/shop' component={ShopPage} />
+                    <Route exact path='/checkout' component={CheckoutPage} />
                     <Route
                         exact
                         path='/signin'
---
+++ src/pages/checkout/checkout.component.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import './checkout.styles.scss'
+import { selectCartItems, selectCartTotal } from '../../redux/cart/cart.selectors'
+import { createStructuredSelector } from 'reselect'
+import { connect } from 'react-redux'
+
+const CheckoutPage = ({ cartItems, total }) => (
+    <div className='checkout-page'>
+        <div className='checkout-header'>
+            <div className='header-block'>
+                <span>Product</span>
+            </div>
+            <div className='header-block'>
+                <span>Description</span>
+            </div>
+            <div className='header-block'>
+                <span>Quantity</span>
+            </div>
+            <div className='header-block'>
+                <span>Price</span>
+            </div>
+            <div className='header-block'>
+                <span>Remove</span>
+            </div>
+        </div>
+    </div>
+)
+
+const mapStateToProps = createStructuredSelector({
+    cartItems: selectCartItems,
+    total: selectCartTotal
+})
+
+export default connect(mapStateToProps)(CheckoutPage)
