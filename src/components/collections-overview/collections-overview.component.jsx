a141f6ae8fee878baf84b8ab8ec1d262a1b3092b
Duy <huynhduytam2210@gmail.com>
2020-09-04 00:49:07 +0700

    Data Flow In Our App

--- src/components/collections-overview/collections-overview.component.jsx
+++ src/components/collections-overview/collections-overview.component.jsx
@@ -2,13 +2,13 @@ import CollectionPreview from '../collection-preview/collection-preview.componen
 import './collections-overview.styles.scss'
 import { connect } from 'react-redux'
 import { createStructuredSelector } from 'reselect'
-import { selectCollections } from '../../redux/shop/shop.selectors'
+import { selectCollectionsForPreview } from '../../redux/shop/shop.selectors'
 import React from 'react'
 
 const CollectionsOverview = ({ collections }) => (
     <div className='collections-overview'>
         {
-        collections.map(({ id, ...otherCollections }) => (
+            collections.map(({ id, ...otherCollections }) => (
                 <CollectionPreview key={id} {...otherCollections} />
             ))
         }
@@ -16,7 +16,7 @@ const CollectionsOverview = ({ collections }) => (
 )
 
 const mapStateToProps = createStructuredSelector({
-    collections: selectCollections
+    collections: selectCollectionsForPreview
 })
 
 export default connect(mapStateToProps)(CollectionsOverview)
