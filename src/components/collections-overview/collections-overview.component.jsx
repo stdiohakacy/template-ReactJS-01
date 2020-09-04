e8b7143f68c2f0026f7a8d13d024ea553db3740a
Duy <huynhduytam2210@gmail.com>
2020-09-04 07:41:23 +0700

    fix

--- src/components/collections-overview/collections-overview.component.jsx
+++ src/components/collections-overview/collections-overview.component.jsx
@@ -1,17 +1,16 @@
-import CollectionPreview from '../collection-preview/collection-preview.component'
-import './collections-overview.styles.scss'
+import React from 'react'
 import { connect } from 'react-redux'
 import { createStructuredSelector } from 'reselect'
+import CollectionPreview from '../collection-preview/collection-preview.component'
 import { selectCollectionsForPreview } from '../../redux/shop/shop.selectors'
-import React from 'react'
+
+import './collections-overview.styles.scss'
 
 const CollectionsOverview = ({ collections }) => (
     <div className='collections-overview'>
-        {
-            collections.map(({ id, ...otherCollections }) => (
-                <CollectionPreview key={id} {...otherCollections} />
-            ))
-        }
+        {collections.map(({ id, ...otherCollectionProps }) => (
+            <CollectionPreview key={id} {...otherCollectionProps} />
+        ))}
     </div>
 )
 
--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Route } from 'react-router-dom'
+
 import CollectionsOverview from '../../components/collections-overview/collections-overview.component'
 import CollectionPage from '../collection/collection.component'
 
